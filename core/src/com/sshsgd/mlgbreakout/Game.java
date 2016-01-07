d225a037ba09e3d311832da0f7fd9368bb3c2480
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-01-06 20:26:34 -0800

    FPS Counter

--- core/src/com/sshsgd/mlgbreakout/Game.java
+++ core/src/com/sshsgd/mlgbreakout/Game.java
@@ -10,10 +10,19 @@ public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
+	public static final String GAME_TITE = "MLG Breakout";
+	
+	private float fpsTime;
+	private int fps, frames;
+	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		frames = 0;
+		fps = 0;
+		fpsTime = 0;
 	}
 
 	@Override
@@ -23,5 +32,17 @@ public class Game extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		
+		float dt = Gdx.graphics.getDeltaTime();
+		
+		fpsTime += dt;
+		frames++;
+		if(fpsTime >= 1) {
+			fps = frames;
+			frames = 0;
+			fpsTime = 0;
+		}
+		
+		Gdx.graphics.setTitle(String.format("%s | FPS: %d", Game.GAME_TITE, fps));
 	}
 }
